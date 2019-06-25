994f07268ab6988f354673c6d782124f2330dd9e
cengiz <cengiz@cengiz-X550VC>
2019-06-25 13:36:00 +0300

    added BookService and Main class

--- src/main/resources/model/model/BookService.java
+++ src/main/resources/model/model/BookService.java
@@ -1,5 +1,63 @@
 package model;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class BookService {
+	
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("bookStorePU");
+	
+	private EntityManager em = emf.createEntityManager();
+	
+	private EntityTransaction tx = em.getTransaction();
+	
+	public Book createBook(Long id, String title,String desc,Float unitCost,String isbn,Integer numOfPages) {
+		
+		Book book = new Book();
+		
+		book.setId(id);
+		book.setTitle(title);
+		book.setDescription(desc);
+		book.setUnitCost(unitCost);
+		book.setIsbn(isbn);
+		book.setNbOfPages(numOfPages);
+
+		tx.begin();
+		em.persist(book);
+		tx.commit();
+		return book;
+		
+	}
+	
+	public Book raiseUnitCost(Long id, Float raise) {
+		
+		Book book = new Book();
+		
+		if(book != null) {
+			
+			tx.begin();
+			book.setUnitCost(book.getUnitCost()+ raise);
+			tx.commit();
+		}
+		
+		return book;
+	}
+	
+	public void removeBook(Long id) {
+		Book book = new Book();
+		
+		if(book != null) {
+			
+			tx.begin();
+			em.remove(book);
+			tx.commit();
+		}
+	}
+	
+	public Book findBook(Long id) {
+		return em.find(Book.class,id);
+	}
 
 }
--- src/main/resources/model/model/Main.java
+++ src/main/resources/model/model/Main.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Main {
+	
+  public static void main(String[] args) {
+	
+	  BookService bookService = new BookService();
+	  
+	  Book book = bookService.createBook(4004L,"h2g2","Scfi Book", 12.5f,"1234",738);
+	  
+	  System.out.println("Book persisted :"+book);
+	  
+	  book = bookService.findBook(404L);
+	  
+	  System.out.println("Book found: "+book);
+	  
+	  book = bookService.raiseUnitCost(404L,13.5f);
+	  
+	  System.out.println("Book updated : "+book);
+	  
+	  
+}
 
 }
